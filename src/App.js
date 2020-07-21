ee6afd617f999a0b51c91e829ca0afe5c8f3bc61
Vincent Odhiambo <ovincent7949@gmail.com>
2020-07-21 09:17:20 +0300

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-blue-200 flex items-center justify-center">
+      <div>this is some text</div>
+    </div>
+  );
+}
+
+export default App;
